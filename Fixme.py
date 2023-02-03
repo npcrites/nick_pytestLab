336c434013af5a261f9c7c53521fe41f4ed3f68f
Your Name <cameronshir11@gmail.com>
2023-02-03 12:12:12 -0800

    Is it good

--- Fixme.py
+++ Fixme.py
@@ -5,4 +5,4 @@ def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
     '''
-    return[x for x in range(n + 1) if x % 2 == 0]
+    return [x for x in range(n + 1) if x % 2 == 0]
